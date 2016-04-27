522c5988794b190651f7443b751f1db09649428b
dingwei <dingwei@didichuxing.com>
2016-04-27 10:15:06 +0800

    add redis

---
+++ src/main/listen.go
@@ -0,0 +1,40 @@
+package main
+import (
+	"fmt"
+	"GoRedisService"
+	"strconv"
+)
+
+func main () {
+	defer GoRedisService.CloseRedis();
+	fmt.Println("listen the world!")
+	GoRedisService.OpenRedis("localhost","6379")
+
+
+	for  {
+
+		if GoRedisService.HGetValue("Product1") == "100" {
+			break
+		}
+
+		popValue := GoRedisService.RPopValue("list")
+		if popValue != "" {
+			fmt.Print("popValue:"+popValue)
+			//fmt.Print("\n")
+			if GoRedisService.HGetValue("Product1") == "" {
+				GoRedisService.HSetValue("Product1","1")
+				//fmt.Print("if here")
+			}else {
+				//fmt.Print("else here")
+				tmp :=GoRedisService.HGetValue("Product1")
+				//fmt.Print("  "+tmp)
+				//fmt.Print("\n")
+				b,_ := strconv.Atoi(tmp);
+				GoRedisService.HSetValue("Product1",strconv.Itoa(b+1))
+			}
+			//fmt.Print(popValue + "\n")
+		}
+	}
+
+
+}
--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"GoRedisService"
+	"strconv"
+	"time"
+)
 
 func main () {
+	//defer GoRedisService.CloseRedis();
 	fmt.Println("Hello World")
+	//GoRedisService.OpenRedis("localhost","6379")
+	//GoRedisService.LPushValue("list","1 ")
+	//GoRedisService.LPushValue("list","2 ")
+	//GoRedisService.LPushValue("list","3 ")
+
+	for index := 100; index > 0; index-- {
+		go func(i int) {
+			defer GoRedisService.CloseRedis();
+			GoRedisService.OpenRedis("localhost","6379")
+			GoRedisService.LPushValue("list",strconv.Itoa(i))
+		}(index)
+		//GoRedisService.LPushValue("list",strconv.Itoa(index))
+	}
+
+
+	//popValue := GoRedisService.RPopValue("list")
+	//for  popValue != ""{
+	//	fmt.Print(popValue+"\n")
+	//	popValue = GoRedisService.RPopValue("list")
+	//}
+
+	time.Sleep(10*time.Second)
+
 }

dbb7bb9a0fc76c1204dcad1d863c5fc5b12f5be8
dongnan <dongnan@didichuxing.com>
2016-04-27 14:39:02 +0800

    test

--- src/main/configTest.go
+++ src/main/configTest.go
@@ -1,1 +1,17 @@
 package main
+import (
+	"fmt"
+	"os"
+	"util"
+)
+
+func main() {
+	myConfig := new(util.Config)
+	path,_ := os.Getwd()
+	fmt.Println(path)
+	path += "/src/util/config.txt"
+	fmt.Println(path)
+	myConfig.InitConfig(path)
+	fmt.Println(myConfig.Read("default", "path"))
+	fmt.Printf("%v", myConfig.Mymap)
+}

25e4cb7356b762882dd4848db5db5832f9fc7d2e
xueliang <xueliang@didichuxing.com>
2016-04-27 15:39:34 +0800

    lulei

--- src/controller/seckillingController.go
+++ src/controller/seckillingController.go
@@ -6,22 +6,26 @@ import (
 	"strconv"
 	"vo"
 	"encoding/json"
+	"strings"
 )
 
+var counter = 0
 func Seckilling(resp http.ResponseWriter, req *http.Request){
 	req.ParseForm()  //解析参数，默认是不会解析的
 	//fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
 	//fmt.Println("path", r.URL.Path)
 	//fmt.Println("scheme", r.URL.Scheme)
 	//fmt.Println(r.Form["url_long"])
-	//for k, v := range req.Form {
-	//	fmt.Println("key:", k)
-	//	fmt.Fprintf(resp, k)
-	//	fmt.Println("val:", strings.Join(v, ""))
-	//	fmt.Fprintf(resp, ":")
-	//	fmt.Fprintf(resp, strings.Join(v, ""))
-	//	fmt.Fprintf(resp, "\n")
-	//}
+	counter += 1
+	fmt.Println(counter)
+	for k, v := range req.Form {
+		fmt.Println("key:", k)
+		fmt.Fprintf(resp, k)
+		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Fprintf(resp, ":")
+		fmt.Fprintf(resp, strings.Join(v, ""))
+		fmt.Fprintf(resp, "\n")
+	}
 	message := &vo.ReturnMsg{0, ""}
 	//test dw
 	defer GoRedisService.CloseRedis();
@@ -35,7 +39,19 @@ func Seckilling(resp http.ResponseWriter, req *http.Request){
 		//fmt.Fprintf(resp, "失败")
 		//return message
 	} else {
-		entry := &vo.QueueEntry{"1", "2", "3"}
+		entry := &vo.QueueEntry{"", "", ""}
+		for key, value := range req.Form {
+			switch key {
+			case "userid" :
+				entry.SetUserid(strings.Join(value, ""))
+			case "productid" :
+				entry.SetProductid(strings.Join(value, ""))
+			default:
+				fmt.Fprintf(resp, "出错了")
+				return
+			}
+		}
+
 		if str, err := json.Marshal(entry); err == nil {
 			GoRedisService.LPushValue("list", string(str))
 			message.SetErrno(0)

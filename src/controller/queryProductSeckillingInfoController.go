e9ed14d09016c25431428442b672a7a9518b5c9d
xueliang <xueliang@didichuxing.com>
2016-04-27 10:02:24 +0800

    interfaca

--- src/controller/queryProductSeckillingInfoController.go
+++ src/controller/queryProductSeckillingInfoController.go
@@ -6,19 +6,19 @@ import (
 "strings"
 )
 
-func QueryProductSeckillingInfo(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()  //解析参数，默认是不会解析的
+func QueryProductSeckillingInfo(resp http.ResponseWriter, req *http.Request) {
+	req.ParseForm()  //解析参数，默认是不会解析的
 	//fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
 	//fmt.Println("path", r.URL.Path)
 	//fmt.Println("scheme", r.URL.Scheme)
 	//fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
+	for k, v := range req.Form {
 		fmt.Println("key:", k)
-		fmt.Fprintf(w, k)
+		fmt.Fprintf(resp, k)
 		fmt.Println("val:", strings.Join(v, ""))
-		fmt.Fprintf(w, ":")
-		fmt.Fprintf(w, strings.Join(v, ""))
-		fmt.Fprintf(w, "\n")
+		fmt.Fprintf(resp, ":")
+		fmt.Fprintf(resp, strings.Join(v, ""))
+		fmt.Fprintf(resp, "\n")
 	}
 	//fmt.Fprintf(w, "Hello World!") //这个写入到w的是输出到客户端的
 }

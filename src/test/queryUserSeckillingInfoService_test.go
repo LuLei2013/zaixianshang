d37a7b03bf5f8554488970d91bf17450eac838db
lulei <lulei@didichuxing.com>
2016-04-29 01:15:13 +0800

    queryUserSeckillingInfo 单侧 +1

--- src/test/queryUserSeckillingInfoService_test.go
+++ src/test/queryUserSeckillingInfoService_test.go
@@ -28,6 +28,7 @@ func TestQueryUserSeckillingInfo_2(t *testing.T){
 	service.QueryUserSeckillingInfo(userid,productid)
 	t.Error("Test_QueryUserSeckillingInfo_1 只卖 "+vo.Product1_Query_Name+" 商品，测试失败") 
 }
+
 // userid正常，秒杀成功，依赖 redis 中有userid=1,已经秒中了productid=111,redis 通过 conf/properties配置
 func TestQueryUserSeckillingInfo_3(t *testing.T){
 	userid,productid :="1","111"
@@ -40,7 +41,7 @@ func TestQueryUserSeckillingInfo_3(t *testing.T){
 	}
 }
 
-// userid正常，秒杀中，依赖 redis  不存在userid=2,productid=111的商品还没卖完,redis 通过 conf/properties
+// userid正常，秒杀中，依赖 redis  不存在userid=2且productid=111的商品还没卖完,redis 通过 conf/properties
 func TestQueryUserSeckillingInfo_4(t *testing.T){
 
 	userid,productid :="2","111"
@@ -54,7 +55,15 @@ func TestQueryUserSeckillingInfo_4(t *testing.T){
 
 }
 
+// userid正常，秒杀失败，依赖 redis  不存在userid=1000，且 productid=111的商品已经卖完,redis 通过 conf/properties
 func TestQueryUserSeckillingInfo_5(t *testing.T){
 
-
+	userid,productid :="1000","111"
+	message := &vo.ResultPersonMsg{0, "", ""}
+	message = service.QueryUserSeckillingInfo (userid,productid)
+	if 0 == message.GetErrno() && "2" == message.GetStatus(){
+		t.Log("商品秒杀失败 测试通过")
+	} else {
+		t.Error("商品秒杀失败 测试失败")
+	}
 }

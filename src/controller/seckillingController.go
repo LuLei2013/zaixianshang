0b7c8ad3ff68d9fd54c18e4e3137d3e47911f8dc
xueliang <xueliang@didichuxing.com>
2016-04-28 14:17:10 +0800

    exception

--- src/controller/seckillingController.go
+++ src/controller/seckillingController.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 	"service"
+	"vo"
 )
 
 var counter = 0
@@ -14,57 +15,22 @@ func Seckilling(resp http.ResponseWriter, req *http.Request) {
 	req.ParseForm() //解析参数，默认是不会解析的
 	counter += 1
 	fmt.Println(counter)
+	message := &vo.ReturnMsg{0, ""}
+	defer func() {//异常处理
+		if err := recover(); err != nil {
+			message.SetErrno(1)
+			message.SetErrMsg("出错了！")
+		}
+		if jsonstr, jsonerr := json.Marshal(message); jsonerr == nil {
+			fmt.Fprintf(resp, string(jsonstr))
+		}
+	}()
 	for k, v := range req.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	message := service.ServiceSeckilling(req)
+	message = service.ServiceSeckilling(req)
 	if jsonstr, jsonerr := json.Marshal(message); jsonerr == nil {
 		fmt.Fprintf(resp, string(jsonstr))
 	}
-	//test dw
-
-
-	//value, _ := redis.RedisPoolOne.Get(vo.Product1_Query_Name)
-	//if count, _ := strconv.Atoi(string(value)); count >= vo.Product1_Max_Num {
-	//	message.SetErrno(1)
-	//	message.SetErrMsg("秒杀失败")
-	//	if jsonstr, jsonerr := json.Marshal(message); jsonerr == nil {
-	//		fmt.Fprintf(resp, string(jsonstr))
-	//	}
-	//} else {
-	//	entry := &vo.QueueEntry{"", "", ""}
-	//	for key, value := range req.Form {
-	//		switch key {
-	//		case "userid":
-	//			entry.SetUserid(strings.Join(value, ""))
-	//		case "productid":
-	//			entry.SetProductid(strings.Join(value, ""))
-	//		default:
-	//			fmt.Fprintf(resp, "出错了")
-	//			return
-	//		}
-	//	}
-	//
-	//	if str, err := json.Marshal(entry); err == nil {
-	//		err := redis.RedisPoolOne.RPush("list", string(str))
-	//		if err != nil {
-	//			fmt.Println("errMsg:", err)
-	//			return
-	//		}
-	//		message.SetErrno(0)
-	//		message.SetErrMsg("秒杀中")
-	//		if jsonstr, jsonerr := json.Marshal(message); jsonerr == nil {
-	//			fmt.Fprintf(resp, string(jsonstr))
-	//		}
-	//	} else {
-	//		message.SetErrno(1)
-	//		message.SetErrMsg("参数出错")
-	//		if jsonstr, jsonerr := json.Marshal(message); jsonerr == nil {
-	//			fmt.Fprintf(resp, string(jsonstr))
-	//		}
-	//	}
-	//	//return message
-	//}
-	//fmt.Fprintf(w, "Hello World!") //这个写入到w的是输出到客户端的
 }
--- src/service/seckillingService.go
+++ src/service/seckillingService.go
@@ -13,14 +13,14 @@ import (
 func ServiceSeckilling(req *http.Request) *vo.ReturnMsg {
 	message := &vo.ReturnMsg{0, ""} //返回消息
 	if !vo.Flag {
-		message.SetErrno(3)
+		message.SetErrno(0)
 		message.SetErrMsg("秒杀失败")
 		return message
 	}
 	if value, err := redis.RedisPoolOne.Get(vo.Product1_Query_Name); err == nil {
 		if count, _ := strconv.Atoi(string(value)); count >= vo.Product1_Max_Num {
 			vo.Flag = false
-			message.SetErrno(3)
+			message.SetErrno(0)
 			message.SetErrMsg("秒杀失败")
 		} else {
 			req.ParseForm() //解析参数，默认是不会解析的
@@ -34,9 +34,8 @@ func ServiceSeckilling(req *http.Request) *vo.ReturnMsg {
 					if (strValue == vo.Product1_Query_Name || strValue == vo.Product2_Query_Name || strValue == vo.Product3_Query_Name) {
 						entry.SetProductid(strValue)
 					} else {
-						message.SetErrno(1)
-						message.SetErrMsg("参数错误")
-						return message
+						fmt.Println("errMsg:", "productid不存在")
+						panic("productid不存在")
 					}
 				}
 				default:
@@ -44,28 +43,25 @@ func ServiceSeckilling(req *http.Request) *vo.ReturnMsg {
 				}
 			}
 			if entry.GetUserid() == "" || entry.GetProductid() == "" {
-				message.SetErrno(1)
-				message.SetErrMsg("参数错误")
-				return message
+				fmt.Println("errMsg:", "参数错误")
+				panic("参数错误")
 			}
 			if str, err := json.Marshal(entry); err == nil {
-				err := redis.RedisPoolOne.RPush("list", string(str))
-				if err != nil {
-					fmt.Println("errMsg:", err)
-					message.SetErrno(2)
-					message.SetErrMsg("Redis链接错误")
-					return message
+				redisError := redis.RedisPoolOne.RPush("list", string(str))
+				if redisError != nil {
+					fmt.Println("errMsg:", redisError)
+					panic(redisError.Error())
 				}
 				message.SetErrno(0)
 				message.SetErrMsg("秒杀中，请稍后查询")
 			} else {
-				message.SetErrno(1)
-				message.SetErrMsg("参数错误")
+				fmt.Println("errMsg:", err)
+				panic(err.Error())
 			}
 		}
 	} else {
-		message.SetErrno(2)
-		message.SetErrMsg("Redis链接错误")
+		fmt.Println("errMsg:", err)
+		panic(err.Error())
 	}
 	return message
 }
